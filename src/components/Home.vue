6034d69a43d291175ed7c0a2cb18270387d107db
Piyush Goyani <piyush.goyani@corp.avinashi.com>
2019-02-06 13:46:13 +0530

    PWA Template init

---
+++ src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="Home">
+    <h1>PWA Boilerplate</h1>
+    <img src="../assets/logo.png" alt="Vue.js PWA">
+    <a href="/Home">Home</a>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home'
+  }
+</script>
+
